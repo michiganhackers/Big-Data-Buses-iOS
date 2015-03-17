31fad4e299c0548b5d65d46e20d9889428b8f1bb
Ayush Mehra <ayush.mehra@gmail.com>
2015-03-16 20:12:46 -0400

    first commit

---
+++ BigDataBuses/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  BigDataBuses
+//
+//  Created by Ayush Mehra on 3/12/15.
+//  Copyright (c) 2015 amehra. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let span = MKCoordinateSpanMake(0.05, 0.05)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        println("locations = \(locValue.latitude) \(locValue.longitude)")
+        mapView.setRegion(MKCoordinateRegion(center: locValue, span: span),animated: true)
+        
+        locationManager.stopUpdatingLocation() //UPDATE THIS; ONLY UPDATES LOCATION ONCE
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
