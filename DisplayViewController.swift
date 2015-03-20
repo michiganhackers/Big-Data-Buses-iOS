630679dd38080595fa03fea32bb686243e08cd28
vaishr123 <vaishr@umich.edu>
2015-03-20 00:15:46 -0400

    container view with embedded page view controller

---
+++ DisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DisplayViewController.swift
+//  BigDataBuses
+//
+//  Created by Vaish Raman on 3/19/15.
+//  Copyright (c) 2015 amehra. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
